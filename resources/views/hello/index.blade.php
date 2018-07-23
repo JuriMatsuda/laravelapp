8423d437112408d335d066c06caec8191fcc63a5
Juri Matsuda <matsuda.juri@gmail.com>
2018-07-23 23:13:48 +0900

    use blade

---
+++ resources/views/hello/index.blade.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+  <title>Hello</title>
+  <style>
+    body {
+      font-size: 16pt;
+      color: #999;
+    }
+    h1 {
+      font-size: 50pt;
+      text-align: right;
+      color: #f6f6f6;
+      margin: -20px 0 -30px 0;
+    }
+  </style>
+</head>
+<body>
+  <h1>Blade/Index</h1>
+  <p>{{$msg}}</p>
+  </body>
+</html>
