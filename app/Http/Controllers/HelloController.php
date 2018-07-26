5263c97d5b88944d3405a67403625998931fd261
Juri Matsuda <matsuda.juri@gmail.com>
2018-07-26 21:29:44 +0900

    use view composer

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -40,7 +40,7 @@ class HelloController extends Controller {
 //   }
 
   public function index() {
-        return view('hello.index');
+        return view('hello.index', ['message' => 'Hello!']);
   }
 
   public function post(Request $request) {
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,34 +1,15 @@
-<html>
-<head>
-  <title>Hello</title>
-  <style>
-    body {
-      font-size: 16pt;
-      color: #999;
-    }
-    h1 {
-      font-size: 50pt;
-      text-align: right;
-      color: #f6f6f6;
-      margin: -20px 0 -30px 0;
-    }
-  </style>
-</head>
-<body>
-  <h1>Blade/Index</h1>
-  @isset ($msg)
-    <p>
-      こんにちは、{{$msg}}さん。
-    </p>
-  @else
-  <p>
-    何か書いてください。
-  </p>
-  @endisset
-  <form method="post" action="/hello">
-    {{ csrf_field() }}
-    <input type="text" name="msg">
-    <input type="submit">
-  </form>
-</body>
-</html>
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+@section('menubar')
+  @parent
+  インデックスページ
+@endsection
+
+@section('content')
+  <p>ここが本文のコンテンツです。</p>
+  <p>Controller value<br>'message' = {{$message}}</p>
+  <p>ViewComposer value<br>'view_message' = {{$view_message}}</p>
+@section('footer')
+  copyright 2018 nchaaa
+@endsection
