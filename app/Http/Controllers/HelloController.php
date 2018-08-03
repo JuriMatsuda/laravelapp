776f9b4481b49dff2c0325de778611d4a646c4e5
Juri Matsuda <matsuda.juri@gmail.com>
2018-08-03 22:44:18 +0900

    create Validator

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Dotenv\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
@@ -16,6 +17,23 @@ class HelloController extends Controller
 
     public function post(HelloRequest $request)
     {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between0,150',
+        ]);
+
+        /**
+         *  fails()はバリデーションチェックに失敗したらtrue（エラー時のメッセージを表示）
+         *  逆はpasses()。（バリデーションをパスしていたらtrue）
+         *
+         *  withErrors()はリダイレクト時に引数のエラーメッセージを引き継がす。
+         *  withInput()で直前の送信されたフォームの値を残す。
+         */
+        if ($validator->fails()) {
+            return redirect('/hello')->withErrors($validator)->withInput();
+        }
         return view('hello.index', ['msg' => '正しく入力されました。']);
     }
 }
