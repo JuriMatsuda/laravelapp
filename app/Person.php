ed48861fd77703a5ddd6ba09dfd0b29541844bc9
Juri Matsuda <matsuda.juri@gmail.com>
2018-09-05 22:05:03 +0900

    modify PersonModel

--- app/Person.php
+++ app/Person.php
@@ -13,6 +13,14 @@ class Person extends Model
         static::addGlobalScope(new ScopePerson());
     }
 
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    );
+
     public function getData()
     {
         return $this->id . ':' . $this->name . '(' . $this->age . ')';
