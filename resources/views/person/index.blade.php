4b2a4bfad0a28d0e4962efbcccf47beee592a870
Juri Matsuda <matsuda.juri@gmail.com>
2018-09-10 21:40:20 +0900

    modify person.index

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -10,15 +10,17 @@
 @section('content')
     <table>
         <tr>
-            <th>Name</th>
-            <th>Mail</th>
-            <th>Age</th>
+            <th>Person</th>
+            <th>Board</th>
         </tr>
         @foreach($items as $item)
             <tr>
-                <td>{{$item->name}}</td>
-                <td>{{$item->mail}}</td>
-                <td>{{$item->age}}</td>
+                <td>{{$item->getData()}}</td>
+                <td>
+                    @if($item->board != null)
+                        {{$item->board->getData()}}
+                    @endif
+                </td>
             </tr>
         @endforeach
     </table>
