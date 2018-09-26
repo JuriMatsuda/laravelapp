144b96c985389f8a950c2031989944ff0b0fda81
Juri Matsuda <matsuda.juri@gmail.com>
2018-09-26 22:28:34 +0900

    embed form

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::get('hello/edit', 'HelloController@edit');
 Route::post('hello/edit', 'HelloController@update');
 Route::get('hello/del', 'HelloController@del');
 Route::post('hello/del', 'HelloController@remove');
+Route::get('hello/rest', 'HelloController@rest');
 
 Route::get('person', 'PersonController@index');
 Route::get('person/find', 'PersonController@find');
